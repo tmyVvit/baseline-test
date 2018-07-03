2467f7f1be3cd3e8aa4f8b79b25d0ce5b8f597c6
terry.teng@oocl.com <Ilovetmyy1>
2018-07-03 14:36:35 +0800

    [Terry] practice 2

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -2,15 +2,12 @@
 
 function collectSameElements(collectionA, collectionB) {
   var ans = new Array();
-  var index = 0;
+  var c = collectionB.toString();
   for(var i = 0; i < collectionA.length; i++){
-    for(var j = 0; j < collectionB.length; j++){
-      if(collectionA[i] == collectionB[j]){
-        ans[index] = collectionA[i];
-        index += 1;
-        break;
-      }
+    if(c.indexOf(collectionA[i].toString())>-1){
+      ans.push(collecionA[i]);
     }
   }
+
   return ans;
 }
--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,12 @@
-'use strict';
+﻿'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var ans = new Array();
+  var c = collectionA.toString();
+  for(var i = 0; i < collectionB[0].length; i++){
+    if(c.indexOf(collectionB[0][i].toString())>-1){
+      ans.push(collectionB[0][i]);
+    }
+  }
+  return ans;
 }
